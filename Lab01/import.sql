2adbc2d1e7de42b3618f18c8a36fd07a5e129c9a
WALHAY <walhayggwp@gmail.com>
2025-09-11 23:30:48 +0300

    Fix import

--- Lab01/import.sql
+++ Lab01/import.sql
@@ -1,10 +1,11 @@
-COPY library.users FROM "/var/lib/postgresql/data/users.csv" DELIMETER ',' CSV HEADER;
+COPY library.users FROM '/var/lib/postgresql/data/users.csv' DELIMITER ',' CSV HEADER;
 
-COPY library.games FROM "/var/lib/postgresql/games.csv" DELIMETER ',' CSV HEADER;
+COPY library.games FROM '/var/lib/postgresql/data/games.csv' DELIMITER ',' CSV HEADER;
 
-COPY library.publishers FROM "/var/lib/postgresql/publishers.csv" DELIMETER ',' CSV HEADER;
+COPY library.publishers FROM '/var/lib/postgresql/data/publishers.csv' DELIMITER ',' CSV HEADER;
 
-COPY library.genres FROM "/var/lib/postgresql/genres.csv" DELIMETER ',' CSV HEADER;
+COPY library.genres FROM '/var/lib/postgresql/data/genres.csv' DELIMITER ',' CSV HEADER;
 
-COPY library.users FROM "/var/lib/postgresql/users.csv" DELIMETER ',' CSV HEADER;
+COPY library.users_games FROM '/var/lib/postgresql/data/users_games.csv' DELIMITER ',' CSV HEADER;
 
+COPY library.publishers_games_genres FROM '/var/lib/postgresql/data/publishers_games_genres.csv' DELIMITER ',' CSV HEADER;
